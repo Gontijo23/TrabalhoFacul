e7343a4ac2fc7228227c97fba05415505cc9d8d0
Gontijo23 <gabgg7879@gmail.com>
2022-10-19 22:41:32 -0300

    Primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FavoritosComponent } from './pages/favoritos/favoritos.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { SeusIngressosComponent } from './pages/seus-ingressos/seus-ingressos.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"menu", component: MenuComponent},
+  {path:"perfil", component: PerfilComponent},
+  {path:"seus-ingressos", component: SeusIngressosComponent},
+  {path:"sobre", component: SobreComponent},
+  {path:"favoritos", component: FavoritosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sharepage/sidebar/sidebar.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { SeusIngressosComponent } from './pages/seus-ingressos/seus-ingressos.component';
+import { FavoritosComponent } from './pages/favoritos/favoritos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    FooterComponent,
+    MenuComponent,
+    HomeComponent,
+    SobreComponent,
+    PerfilComponent,
+    SeusIngressosComponent,
+    FavoritosComponent
   ],
   imports: [
     BrowserModule,
